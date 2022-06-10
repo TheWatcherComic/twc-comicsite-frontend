05f2b70e698e916857e0b66cebea7e3bb3c000e5
Albeniz Vargas <albeniz1111@gmail.com>
2022-06-09 23:23:59 -0500

    Bug fixes

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import CardLanding from './Card';
+import ComicList from "./ComicsList";
+
 
 
 const LandingSection = (props) => {
-    let array = props.children.comicinfo;
+    let array = ComicList;
     let arrtag = props.children.tag;
     let arrhouse = props.children.house;
     let newarray = [];
-
+    
 
     array.forEach(array => {
         if(array.tag === arrtag || array.house === arrhouse){
@@ -16,7 +18,7 @@ const LandingSection = (props) => {
     });
 
   return (
-    <div class="py-6 px-4 sm:p-6 md:py-10 md:px-40  ">
+    <div class="py-6 px-4 sm:p-6 md:py-10 md:px-40 ">
         <div className='flex flex-row justify-between items-end'>
             <h1 className='font-extrabold text-3xl mb-5 dark:text-white'>{props.children.titulo}</h1>
             <a className='font-extrabold text-sm mb-5 dark:text-red-500 text-red-500 hover:text-red-400 cursor-pointer transition duration-300'>See more..</a>
