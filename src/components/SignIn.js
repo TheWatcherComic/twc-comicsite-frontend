6ac07d2b7522ab52abaa30ea4e813df55ca117a6
Albeniz Vargas <albeniz1111@gmail.com>
2022-06-12 15:22:43 -0500

    Fixing landing card

---
+++ src/components/SignIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import LandingSection from "./LandingSection";
+
+
+function SignIn() {
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default SignIn;
