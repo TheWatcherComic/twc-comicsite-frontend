613f37df3907eda8cba8a551397d3304391019b3
Albeniz Vargas <albeniz1111@gmail.com>
2022-07-22 10:22:44 -0500

    dropDown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,24 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       await logout();
-    navigate("/sign-in");
+      navigate("/sign-in");
     } catch (error) {
       console.log(error);
     }
   };
+
+  const handleDropDown =()=>{
+    var dropDown = document.getElementById("dropDown");
+    if(dropDown.classList.contains("block")){
+      dropDown.classList.add("hidden");
+      dropDown.classList.remove("block");
+    }else if(dropDown.classList.contains("hidden")){
+      dropDown.classList.add("block");
+      dropDown.classList.remove("hidden");
+    }
+  }
+  const defaultPic =
+    "https://icon-library.com/images/default-profile-icon/default-profile-icon-24.jpg";
 
   return (
     <>
@@ -63,27 +76,90 @@ const Navbar = () => {
                 <Link to={link.link}>{link.name}</Link>
               </li>
             ))}
-            <li class="md:ml-8 text-base md:my-0 my-7 dark:text-white">
-              {user.displayName || user.email}
-            </li>
-            <li class="md:ml-8 text-base md:my-0 my-7 rounded-full">
-              <img
-                id="avatarButton"
-                class="w-10 h-10 rounded-full border border-red-500"
-                src={user.photoURL}
-                alt="User dropdown"
-              ></img>
-              
+            <li className="md:ml-8 text-base md:my-0 my-7 dark:text-white">
+              <div class="flex justify-center">
+                <div class="relative inline-block ">
+                  <button class="relative z-10 flex items-center p-2 text-sm text-gray-600 bg-white border border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:text-white dark:bg-gray-800 focus:outline-none"
+                  onClick={handleDropDown}
+                  >
+                    <span class="mx-1">{user.displayName || user.email}</span>
+                    <svg
+                      class="w-5 h-5 mx-1"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 15.713L18.01 9.70299L16.597 8.28799L12 12.888L7.40399 8.28799L5.98999 9.70199L12 15.713Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <div class="absolute right-10 z-20 w-56 py-2 mt-5 overflow-hidden bg-white rounded-md shadow-xl dark:bg-gray-800 hidden" id="dropDown">
+                <a
+                  href="#"
+                  class="flex items-center p-3 -mt-2 text-sm text-gray-600 transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  <img
+                    class="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9"
+                    src={user.photoURL || defaultPic}
+                    alt="jane avatar"
+                  ></img>
+                  <div class="mx-1">
+                    <h1 class="text-sm font-semibold text-gray-700 dark:text-gray-200">
+                    {user.displayName || user.email}
+                    </h1>
+                    <p class="text-sm text-gray-500 dark:text-gray-400">
+                    {user.email}
+                    </p>
+                  </div>
+                </a>
+
+                <hr class="border-gray-200 dark:border-gray-700 " />
+
+                <a
+                  href="#"
+                  class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  view profile
+                </a>
+
+                <a
+                  href="#"
+                  class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Settings
+                </a>
+
+                <a
+                  href="#"
+                  class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Keyboard shortcuts
+                </a>
+
+                <hr class="border-gray-200 dark:border-gray-700 " />
+
+                <a
+                  href="#"
+                  class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Help
+                </a>
+                <button
+                  onClick={handleLogout}
+                  href="#"
+                  class="block px-4 py-3 w-full text-left text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Sign Out
+                </button>
+              </div>
             </li>
 
-            <li className="md:ml-8 text-base md:my-0 my-7 dark:text-white">
-              <button
-                className="bg-red-500 text-white py-2 px-6 rounded-lg md:ml-8 hover:bg-red-400 duration-500"
-                onClick={handleLogout}
-              >
-                Log-out
-              </button>
-            </li>
+
           </ul>
         </div>
       </div>
